6a24a564304917031f80cfca25106386ec3350d4
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-04-05 12:53:50 -0700

    remove string from initializer

--- lib/mobilize-ssh.rb
+++ lib/mobilize-ssh.rb
@@ -6,7 +6,6 @@ require "net/scp"
 require "mobilize-ssh/extensions/net-ssh-connection-session"
 require "mobilize-ssh/extensions/net-ssh-gateway"
 require "mobilize-ssh/extensions/socket"
-require "mobilize-ssh/extensions/string"
 
 module Mobilize
   module Ssh
